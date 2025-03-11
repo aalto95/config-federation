505926d403c6dd65a2b204b93453e3bd9f8783a0
aalto95 <stanportableuse@gmail.com>
2025-03-11 15:19:38 +0900

    Fix eslintConfig type

--- vue/lib/index.d.ts
+++ vue/lib/index.d.ts
@@ -3,7 +3,7 @@ import { Config as PrettierConfig } from "prettier";
 import { Config as StylelintConfig } from "stylelint";
 
 declare module "@config-federation/vue" {
-  export const eslintConfig: EslintConfig;
+  export const eslintConfig: EslintConfig[];
   export const prettierConfig: PrettierConfig;
   export const stylelintConfig: StylelintConfig;
   export function deepMerge(config: object, customs: object): object;
