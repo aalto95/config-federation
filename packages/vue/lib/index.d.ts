52a4810e535b2c16de9ccbe401c40f1c29fb2b34
aalto95 <43157835+aalto95@users.noreply.github.com>
2026-04-24 15:22:57 +0900

    Update config names

--- packages/vue/lib/index.d.ts
+++ packages/vue/lib/index.d.ts
@@ -1,13 +1,13 @@
 import type { Config as PrettierConfig } from "prettier";
-import type { Config as StylelintConfig } from "stylelint";
+import type StylelintConfig from "stylelint";
 import type { Config as EslintConfig } from "typescript-eslint";
-import type { UserConfig as VitestConfig } from "vitest/node";
+import type { TestUserConfig as VitestConfig } from "vitest/node";
 
 export type { EslintConfig, PrettierConfig, StylelintConfig, VitestConfig };
 
 export declare const eslintConfig: EslintConfig[];
 export declare const prettierConfig: PrettierConfig;
-export declare const stylelintConfig: StylelintConfig;
+export declare const stylelintConfig: typeof StylelintConfig;
 export declare const vitestConfig: VitestConfig;
 export function deepMerge<TConfig, TCustoms>(
 	config: TConfig,
